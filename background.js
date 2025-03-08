fbc7534a3326ad198840e6a854a718e57a540406
alon mayaffit <40304260-alonmayafit@users.noreply.replit.com>
2025-03-08 21:08:05 +0000

    fix commit

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('WebSocket Proto Debugger installed');
+    console.log("WebSocket Proto Debugger installed");
 });
 
 // Add debugging capabilities
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log('Background script received message:', message);
-    if (message.type === 'DEBUG_LOG') {
-        console.log('Debug log from panel:', message.data);
+    console.log("Background script received message:", message);
+    if (message.type === "DEBUG_LOG") {
+        console.log("Debug log from panel:", message.data);
     }
     sendResponse({ received: true });
 });
